5b98dff20fc31ab81c3303aa0300c73cc7af9731
rithu453 <marpallyrithwik8@gmail.com>
2024-09-30 23:25:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+
+const [mnemonic, setMnemonic] = useState("");
+
+
+
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
